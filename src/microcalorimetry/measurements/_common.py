46f5b7cfc05600c4b673a9495ffa694fcd4b5e42
rfpower <rfpower@NIST.GOV>
2026-04-22 09:59:29 -0600

    updated DC sweep to support thermometers

--- src/microcalorimetry/measurements/_common.py
+++ src/microcalorimetry/measurements/_common.py
@@ -1,31 +1,35 @@
-from rminstr.utilities.path import new_dir
-from microcalorimetry.math import rfpower
-from rmellipse.uobjects import RMEMeas
-from rmellipse.propagators import RMEProp
-from rminstr.data_structures import ExistingRecord, ExptParameters
+from rminstr.data_structures import ExistingRecord
 from pathlib import Path
-from os.path import join, dirname, basename
-import microcalorimetry.measurements.rfsweep._parser as microparser
-import microcalorimetry.measurements.rfsweep._runner as microrunner
-import microcalorimetry.configs as configs
-import microcalorimetry._helpers._intf_tools as clitools
-from microcalorimetry._tkquick.dtypes import Folder
 import matplotlib.pyplot as plt
-import os
-import numpy as np
-import pandas as pd
-import json
-import click
-from itertools import cycle
-from decimal import Decimal
+from datetime import datetime
 from fnmatch import fnmatch
-
+import numpy as np
 __all__ = ['view']
+
+
+def _apply_tunit(x: np.array, time_units: str, relative: bool)->np.array:
+    out = x
+
+    if relative:
+        out = out - out[0]
+
+    if time_units == 'seconds':
+        return out
+    elif time_units == 'hours':
+        return out/3600
+    elif time_units == 'days':
+        return out/3600/24
+    elif time_units == 'datetime':
+        return np.array([datetime.fromtimestamp(oi) for oi in out])
+    else:
+        raise NotImplementedError(f'{time_units} not implemented.')
 
 def view(
     metadata: Path,
     include_columns:list[str] = ['*'],
     exclude_columns: list[str] = [],
+    time_units: str = 'seconds',
+    relative_time: bool = True
 ) -> tuple[plt.Figure]:
     """
     Generic plot columns in a data record.
@@ -37,12 +41,30 @@ def view(
     include_columns : list[str]
         Columns to include with glob patters. Leave as ['*']
         to include all.
+    time_units : str, optional
+        Options Format
+        --------------
+        {
+        seconds
+            plot in seconds
+        hours
+            plot in hourse
+        datetime
+            plot in datetime
+        }
+    relative_time : bool, optional
+        Use time since start of record on instead of
+        absolute time.
 
     Returns
     -------
     figures : tuple[Figure]
         Tuple of output figures.
     """
+    metadata = Path(metadata)
+    if metadata.is_dir():
+        metadata = [p for p in metadata.glob('*metadata*')][0]
+    meta_dir = metadata.parents[0]
     dr = ExistingRecord(metadata)
 
     d_full = dr.batch_read()
@@ -51,13 +73,22 @@ def view(
     # otherwise, plot each sensors raw time series in a seperate window
     sensor_figs = []
 
+    # to label the x axis as relative or
+    # absolute time
+    relative_or_not = {
+        True:'Relative',
+        False:'Absolute'
+    }[relative_time]
+
+
     for k in d_full:
         fig_i, axs_i = plt.subplots(1, 1)
         fig_i.suptitle(f'{k} : Raw Data')
         ts = d_full[k]
-        axs_i.plot(ts.t, ts.values, 'o-', ds='steps-post')
+        time = _apply_tunit(ts.t,time_units,relative_time)
+        axs_i.plot(time, ts.values, 'o-', ds='steps-post')
         axs_i.set_ylabel(k)
-        axs_i.set_xlabel('Time (s)')
+        axs_i.set_xlabel(f'{relative_or_not} Time ({time_units})')
         sensor_figs.append(fig_i)
 
     return tuple(sensor_figs)
